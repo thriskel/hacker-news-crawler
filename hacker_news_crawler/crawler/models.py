53bed6f896b1252e95802e9accf7b64e4b678d43
thriskel <manuelmartinezrzn@gmail.com>
2024-07-22 01:29:09 +0200

    Adding model and schema for HR posts storage and update.

--- hacker_news_crawler/crawler/models.py
+++ hacker_news_crawler/crawler/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.core import validators
 
-# Create your models here.
+
+class Post(models.Model):
+    rank = models.IntegerField(
+        primary_key=True,
+        validators=[
+            validators.MaxValueValidator(30),
+            validators.MinValueValidator(1)
+        ]
+    )
+    title = models.CharField(
+        verbose_name='Title',
+        max_length=150
+    )
+    title_length = models.IntegerField(
+        validators=[
+            validators.MinValueValidator(0)
+        ]
+    )
+    score = models.IntegerField(
+        validators=[
+            validators.MinValueValidator(0)
+        ]
+    )
+    comments = models.IntegerField(
+        validators=[
+            validators.MinValueValidator(0)
+        ]
+    )
