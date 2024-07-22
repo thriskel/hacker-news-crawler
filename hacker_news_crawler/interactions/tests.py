46e57d95142365d114b6b2be8cc0e686eba4aa67
thriskel <manuelmartinezrzn@gmail.com>
2024-07-22 05:19:54 +0200

    added simple test for interactions

--- hacker_news_crawler/interactions/tests.py
+++ hacker_news_crawler/interactions/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+
+class CrawlerTest(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.interactions_url = '/interactions/'
+
+    def test_list(self):
+        response = self.client.get(self.interactions_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
